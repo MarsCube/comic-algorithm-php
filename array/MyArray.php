cece676f5cc18535931a5c204c3b137adda51621
zouzhipeng <zouzhipeng@ddaishujiankang.com>
2019-10-22 21:24:41 +0800

    resize

--- array/MyArray.php
+++ array/MyArray.php
@@ -21,6 +21,7 @@ class MyArray
             $string .= sprintf('%d => %s; ', $key, $value);
         }
 
+        $string .= ' -- capacity: ' . $this->capacity . ', size: ' . $this->size;
         return $string;
     }
 
@@ -37,15 +38,57 @@ class MyArray
 
         $this->array[$index] = $value;
         $this->size++;
+        if ($this->size >= $this->capacity) {
+            $this->resize($this->capacity * 2);
+        }
+    }
 
+    public function delete(int $index)
+    {
+        if ($index < 0 || $index > $this->capacity - 1) {
+            throw new Exception('out of range');
+        }
+
+        // 注意边界的处理
+        for ($i = $index; $i < $this->size - 1; $i++) {
+            $this->array[$i] = $this->array[$i + 1];
+        }
+
+        $this->size--;
+        if ($this->size < $this->capacity / 2 && $this->capacity / 2 > 0) {
+            $this->resize($this->capacity / 2);
+        }
+    }
+
+    private function resize(int $newCapacity)
+    {
+        $newArray = array_fill(0, $newCapacity, null);
+        for ($i = 0; $i < $this->size; $i++)
+            $newArray[$i] = $this->array[$i];
+
+        $this->array = $newArray;
+        $this->capacity = $newCapacity;
     }
 }
 
-$arr = new MyArray(8);
+$arr = new MyArray(3);
 var_dump($arr->toString());
 $arr->add(0, 12);
 var_dump($arr->toString());
 $arr->add(0, 3);
 var_dump($arr->toString());
-$arr->add(1, 3);
+$arr->add(1, 7);
+var_dump($arr->toString());
+$arr->add(1, 2);
+var_dump($arr->toString());
+$arr->add(1, 66);
+var_dump($arr->toString());
+$arr->add(0, 4);
+var_dump($arr->toString());
+$arr->delete(1);
+var_dump($arr->toString());
+$arr->delete(1);
+$arr->delete(1);
+var_dump($arr->toString());
+$arr->delete(0);
 var_dump($arr->toString());
